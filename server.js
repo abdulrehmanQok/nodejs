adab41f4f2594a363fe2ff2077c81df4ab3b7b01
abdulrehmanQok <abdulrehmanmehmood282@gmail.com>
2024-08-27 16:44:16 +0500

    hhjhk

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import {product} from './data.js'
+import dotenv from 'dotenv';
+dotenv.config();
+const app = express();
+const port=process.env.PORT || 5000;
+app.get('/product',(req,res)=>{
+    res.send(product);
+
+})
+
+app.listen(port,()=>{
+    console.log('server is running on port '+port);
+})
