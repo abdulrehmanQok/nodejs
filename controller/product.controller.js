03062ba2d3036df60dc2998524e9e35a5e274898
abdulrehmanQok <abdulrehmanmehmood282@gmail.com>
2024-08-30 16:02:08 +0500

    product in mongodb

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -4,7 +4,7 @@ export const addproduct = async(req,res)=>{
 try {
     const product = req.body;
 const newproduct = await Product.create(product);
-res.status(201).json({message:"product added successfully",newproduct});
+res.status(200).json({message:"product added successfully",newproduct});
 } catch (error) {
     res.status(500).json({message:"error adding product",error});
 }
